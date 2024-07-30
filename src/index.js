d5c0a2f0d949d9d9d36bb23fa55a381d751d9aef
Neve700 <147994506+Neve700@users.noreply.github.com>
2024-07-30 21:24:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
-}
+        let str = n.toString();
+        let splitString = str.split('');
+        let reverseString = splitString.reverse();
+        return parseInt(reverseString.join(''));
+};
